eda20af5b43a22dee6bf201c0c6c8b141b7376e7
Samson Fagade <sfagade@gmail.com>
2020-03-07 17:48:36 +0100

    created default config for gateway

---
+++ src/main/java/org/tsp/projects/ledar/ledargateway/LedarGatewayApplication.java
@@ -0,0 +1,17 @@
+package org.tsp.projects.ledar.ledargateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class LedarGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedarGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tsp/projects/ledar/ledargateway/LedarGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tsp.projects.ledar.ledargateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedarGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
